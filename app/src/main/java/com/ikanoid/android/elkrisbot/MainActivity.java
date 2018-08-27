8b5ad700de222290157d9c5c50310679d10782f9
Samuel Etsiakoh <esgrey094@gmail.com>
2018-08-27 20:09:43 +0000

    First commit: Built the app and add a material theme which supports all android versions.

---
+++ app/src/main/java/com/ikanoid/android/elkrisbot/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ikanoid.android.elkrisbot;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Making notification bar transparent
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+    }
+}
